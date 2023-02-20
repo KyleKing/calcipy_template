d70458d4f8b91e1d60a121e29986df72518fe497
Kyle King <KyleKing@users.noreply.github.com>
2023-02-20 08:37:22 -0500

    ci: update .ctt output

--- .ctt/default/test_template/__init__.py
+++ .ctt/default/test_template/__init__.py
@@ -1,10 +1,6 @@
 """test_template."""
 
-from loguru import logger
-
 __version__ = '0.0.1'
 __pkg_name__ = 'test_template'
-
-logger.disable(__pkg_name__)
 
 # ====== Above is the recommended code from calcipy_template and may be updated on new releases ======
