6b609fc6868973d342314293c11ef6ce3e8c4951
Kyle King <KyleKing@users.noreply.github.com>
2023-02-22 20:07:44 -0500

    fix: replace shoal references with calcipy and corallium

--- package_template/{{ package_name_py }}/scripts.py
+++ package_template/{{ package_name_py }}/scripts.py
@@ -8,6 +8,6 @@ from . import __pkg_name__, __version__
 @beartype
 def start() -> None:
     """Run the customized Invoke Program."""
-    from shoal.cli import start_program
+    from calcipy.cli import start_program
     from calcipy.tasks import all_tasks
     start_program(__pkg_name__, __version__, all_tasks)
