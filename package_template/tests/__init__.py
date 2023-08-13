12a09d40eed0b507fb46f1c8247b27d4dbedc894
Kyle King <KyleKing@users.noreply.github.com>
2023-08-12 22:48:35 -0400

    fix: allow Runtime Typechecking Mode for tests to be configurable

--- package_template/tests/__init__.py
+++ package_template/tests/__init__.py
@@ -1,3 +1,3 @@
-from os import environ
+from os import environ, getenv
 
-environ['RUNTIME_TYPE_CHECKING_MODE'] = 'ERROR'
+environ['RUNTIME_TYPE_CHECKING_MODE'] = getenv('RUNTIME_TYPE_CHECKING_MODE', 'ERROR')
