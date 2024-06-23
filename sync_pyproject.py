cc38673568a4f8ca049ab5f0c250e0c2d1be2667
Kyle King <KyleKing@users.noreply.github.com>
2024-06-22 20:16:12 -0400

    refactor: improve pyproject sync script

--- sync_pyproject.py
+++ sync_pyproject.py
@@ -7,15 +7,16 @@ import tomlkit
 
 def merge() -> None:
     """Merge ctt output into top-level pyproject.toml."""
-    ctt_pyproject = Path('.ctt/default/pyproject.toml')
+    ctt_pyproject = Path('.ctt/default') / 'pyproject.toml'
     ctt_doc = tomlkit.parse(ctt_pyproject.read_text())
 
     tl_pyproject = Path('pyproject.toml')
     tl_doc = tomlkit.parse(tl_pyproject.read_text())
 
-    for tool in ('commitizen', 'poetry'):
-        ctt_doc['tool'][tool] = tl_doc['tool'][tool]
-    tl_pyproject.write_text(tomlkit.dumps(ctt_doc))
+    for key in ('ruff',):
+        tl_doc['tool'][key] = ctt_doc['tool'][key]
+
+    tl_pyproject.write_text(tomlkit.dumps(tl_doc))
 
 
 if __name__ == '__main__':
