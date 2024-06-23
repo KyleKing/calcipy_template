38fb85bc64910d92594d12a92a8352d315721583
Kyle King <KyleKing@users.noreply.github.com>
2024-06-22 21:54:42 -0400

    refactor: simplify logging for post-setup script

--- package_template/_copier_post_generation.py
+++ package_template/_copier_post_generation.py
@@ -1,34 +1,13 @@
 """Post-Generation Script to be run from Copier."""
 
-import json
 import re
 import shutil
 import sys
-from dataclasses import dataclass
 from pathlib import Path
-
-# Don't print any output if matching directories like:
-# /private/var/folders/1f/gd24l7210d3d8crp0clcm4440000gn/T/copier.main.update_diff.7eb725cw/.git/
-# /private/var/folders/1f/gd24l7210d3d8crp0clcm4440000gn/T/copier.main.recopy_diff.gnos2law/.git/
-_re_copier_dir = re.compile(r'copier\.[^\.]+\.\w+_diff\.')
-_IS_PROJ = not _re_copier_dir.search(Path(__file__).absolute().as_posix())
-
-
-@dataclass
-class Config:
-    cname: str
-    project_description: str
-    package_name_py: str
-    project_name: str
-
-
-_CONFIG_PATH = Path(__file__).with_suffix('.json')
-_CONFIG = Config(**json.loads(_CONFIG_PATH.read_text()))
 
 
 def _log(message: str) -> None:
-    if _IS_PROJ:
-        print(message)  # noqa: T201
+    print(message)  # noqa: T201
 
 
 def cleanup() -> None:
@@ -73,27 +52,10 @@ def validate_configuration() -> None:
 def delete_myself() -> None:
     """Delete this file after completing the main script."""
     Path(__file__).unlink()
-    _CONFIG_PATH.unlink()
 
 
 if __name__ == '__main__':
-    _log(
-        f"""
-The '{_CONFIG.package_name_py}' package has been updated (or created)!
-
-1. Review the changes and commit
-    1. Merge conflicts may either be '*.rej' files or as inline git diffs
-2. Install dependencies with 'poetry install --sync'
-3. Run `./run --help` to show the available actions
-4. Run `./run main --keep-going` to try running all default tasks after the changes
-5. If this is a new project, you could create the GitHub repo with:
-
-    ```sh
-    gh repo create "{_CONFIG.project_name}" --source=. --remote=origin --push \
-        --description="{_CONFIG.project_description}" --homepage="{_CONFIG.cname}" --public
-    ```
-""",
-    )
+    _log('Running self-deleting post-setup script.')
     cleanup()
     validate_configuration()
     delete_myself()
