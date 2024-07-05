3a475510cb291cbac2d78ab281a9cb09fc2798f0
Kyle King <KyleKing@users.noreply.github.com>
2024-07-04 20:05:26 -0400

    ci: merge full ruff config

--- sync_pyproject.py
+++ sync_pyproject.py
@@ -4,10 +4,13 @@ from pathlib import Path
 
 import tomlkit
 
+# PLANNED: use this logic for Configapult when merging dependencies and versions!
+
 
 def merge() -> None:
     """Merge ctt output into top-level pyproject.toml."""
-    ctt_pyproject = Path('.ctt/default') / 'pyproject.toml'
+    ctt_subdir = '.ctt/default'
+    ctt_pyproject = Path(ctt_subdir) / 'pyproject.toml'
     ctt_doc = tomlkit.parse(ctt_pyproject.read_text())
 
     tl_pyproject = Path('pyproject.toml')
@@ -15,6 +18,11 @@ def merge() -> None:
 
     for key in ('ruff',):
         tl_doc['tool'][key] = ctt_doc['tool'][key]
+
+    per_file_ignores = tl_doc['tool']['ruff']['lint']['per-file-ignores']
+    for key in ('tests/*.py',):
+        per_file_ignores[f'package_template/{key}'] = per_file_ignores[key]
+        per_file_ignores[f'{ctt_subdir}/{key}'] = per_file_ignores[key]
 
     tl_pyproject.write_text(tomlkit.dumps(tl_doc))
 
